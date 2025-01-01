1b1e53bd3b37636898e478ad1391f3ef31cb5af3
SanchoPanso <20alexchernik@gmail.com>
2025-01-01 19:51:50 +0300

    feat: update inference example

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,10 @@ import cv2
 from yolo_patch_fusion.wrapper import YOLOInferenceWrapper
 
 
-wrapper = YOLOInferenceWrapper('yolov8.pt')
-image = cv2.imread('image.jpg')
+wrapper = YOLOInferenceWrapper('yolo11n.pt')
+image = cv2.imread('/home/alex/workspace/YOLOPatchFusion/images/zidane3.jpg')
 results = wrapper.detect(image)
 print(results.boxes)
+
+im = results.plot()
+cv2.imwrite('show.jpg', im)
